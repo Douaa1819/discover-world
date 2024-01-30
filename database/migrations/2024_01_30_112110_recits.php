13c653e84e8c86c394bf5c0eb406e97a6c485142
Douaa1819 <douaachemnane@gmail.com>
2024-01-30 13:30:40 +0100

    cree base de donner avec ces tables

---
+++ database/migrations/2024_01_30_112110_recits.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recits', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('description');
+        $table->string('conseil');
+        $table->unsignedBigInteger('id_destination');
+        $table->foreign('id_destination')->references('id')->on('destinations')->onDelete('cascade');
+        $table->unsignedBigInteger('id_image');
+        $table->foreign('id_image')->references('id')->on('images')->onDelete('cascade');
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
