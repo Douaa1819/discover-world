4178b32e6ced336f2b80a4a887371120822aa487
Douaa1819 <douaachemnane@gmail.com>
2024-01-31 15:10:43 +0100

    fixe botton

--- resources/views/components/add-recit.blade.php
+++ resources/views/components/add-recit.blade.php
@@ -5,7 +5,7 @@
     ADD adventure
   </button>
 
-  <div id="crud-modal" tabindex="-1" aria-hidden="true" class=" flex justify-end hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+  <div id="crud-modal" tabindex="-1" aria-hidden="true" class=" hidden justify-end overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
       <div class="relative p-4 w-full max-w-md max-h-full">
           <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
            
