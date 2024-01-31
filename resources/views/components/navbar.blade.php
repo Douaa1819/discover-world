152b16827fdb0fd33302413f252e78a7225c061d
Douaa1819 <douaachemnane@gmail.com>
2024-01-31 10:59:08 +0100

    Update V1

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -24,11 +24,20 @@
             </div>
             <div class="flex items-center space-x-4">
                
-                <a href="Register.php" class="text-blue-800 hover:text-black">Sign Up</a>
-                <a href="Register.php">
-                    <button class="bg-blue-500 text-white px-6 py-2 rounded-md hover:bg-blue-300 transition duration-300">
-                        Sign In
-                    </button>
-                </a> 
+                <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <li>
+                        <a href="/" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        @if (Route::has('login'))
+                        <a href="{{ route('login') }}" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Log in</a>
+                    </li>
+                    <li>
+                        @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Register</a>
+                        @endif
+                        @endauth
+                    </li>
+                </ul>
 </header>
     </body>
